ea1c7c08d8cfb626013925eb6829e0aa01a28ddc
Lunnay1 <doigniesvalentin@gmail.com>
2026-06-08 16:26:22 +0200

    Homemade Pokedex Improved

--- script/pokemon.js
+++ script/pokemon.js
@@ -1,60 +1,26 @@
+const ordinal = ['st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th'];
+const gen = new Map;
+gen.set(1, [1, 151]);
+gen.set(2, [152,251]);
+gen.set(3, [252, 386]);
+gen.set(4, [387, 493]);
+gen.set(5, [494, 649]);
+gen.set(6, [650, 721]);
+gen.set(7, [722, 809]);
+gen.set(8, [810, 906]);
+gen.set(9, [907, 1025]);
+
 function changeGen(string, way) {
     clearPokemon();
-    if (parseInt(string[0]) === 1) {
-        if (way === true) {
-            generatePokemon(152, 251);
-        } else {
-            generatePokemon(906, 1025);
-        }
-    } else if (parseInt(string[0]) === 2) {
-        if (way === true) {
-            generatePokemon(252, 386);
-        } else {
-            generatePokemon(1, 151);
-        }
-    } else if (parseInt(string[0]) === 3) {
-        if (way === true) {
-            generatePokemon(387, 493);
-        } else {
-            generatePokemon(152, 251);
-        }
-    } else if (parseInt(string[0]) === 4) {
-        if (way === true) {
-            generatePokemon(494, 649);
-        } else {
-            generatePokemon(252, 386);
-        }
-    } else if (parseInt(string[0]) === 5) {
-        if (way === true) {
-            generatePokemon(650, 721)
-        } else {
-            generatePokemon(387, 493);
-        }
-    } else if (parseInt(string[0]) === 6) {
-        if (way === true) {
-            generatePokemon(722, 809);
-        } else {
-            generatePokemon(494, 649);
-        }
-    } else if (parseInt(string[0]) === 7) {
-        if (way === true) {
-            generatePokemon(810, 905);
-        } else {
-            generatePokemon(650, 721);
-        }
-    } else if (parseInt(string[0]) === 8) {
-        if (way === true) {
-            generatePokemon(906, 1025);
-        } else {
-            generatePokemon(722, 809);
-        }
-    } else if (parseInt(string[0]) === 9) {
-        if (way === true) {
-            generatePokemon(1, 151);
-        } else {
-            generatePokemon(810, 905);
-        }
-    } else return;
+    let generation = 0;
+    if (way === true) {
+        if (parseInt(string[0]) === 9) generation = gen.get(1);
+        else generation = gen.get(parseInt(string[0])+1);
+    } else {
+        if (parseInt(string[0]) === 1) generation = gen.get(9);
+        else generation = gen.get(parseInt(string[0])-1);
+    }
+    generatePokemon(generation[0], generation[1]); 
 }
 
 function search(value) {
@@ -82,15 +48,12 @@ function search(value) {
         main.appendChild(pokemon_div_new);
         pokemon_div_new.appendChild(pokemon_img);
         if (title) {
-            if (value < 152) title.innerText = "1st Gen Pokemon";
-            else if (value < 252) title.innerText = "2nd Gen Pokemon";
-            else if (value < 387) title.innerText = "3rd Gen Pokemon";
-            else if (value < 494) title.innerText = "4th Gen Pokemon";
-            else if (value < 650) title.innerText = "5th Gen Pokemon";
-            else if (value < 722) title.innerText = "6th Gen Pokemon";
-            else if (value < 810) title.innerText = "7th Gen Pokemon";
-            else if (value < 906 ) title.innerText = "8th Gen Pokemon";
-            else title.innerText = "9th Gen Pokemon";
+            for (const generation of gen) {
+                if (value >= generation[1][0] && value <= generation[1][1]) {
+                    console.log(generation)
+                    title.textContent = `${generation[0]}${ordinal[generation[0]-1]} Gen Pokemon`
+                }
+            }
         }
     }
 }
@@ -105,6 +68,13 @@ function clearPokemon() {
     }
 }
 
+function clearAll() {
+    const main = document.querySelector('.container');
+    if (main) {
+        main.remove();
+    }
+}
+
 function generatePokemon(min, max) {
     const main = document.querySelector('.block-pokemons');
     const title = document.querySelector('h1');
@@ -114,9 +84,23 @@ function generatePokemon(min, max) {
             const pokemon_div = document.createElement('div');
             const pokemon_img = document.createElement('img');
             const pokemon_name = document.createElement('p');
+            pokemon_name.classList.add('bold')
 
             pokemon_div.classList.add('col-xl-1', 'col-lg-2', 'col-md-4', 'col-sm-6', 'col-12');
             pokemon_img.src = urlById(i);
+
+            fetch(`https://pokeapi.co/api/v2/pokemon/${i}`, {method: 'GET'})
+            .then(res => {
+                return res.json();
+            })
+            .then(json => {
+                if (json) {
+                    pokemon_name.textContent = json.name;
+                } else throw new Error("Non-JSON format received. Abort.")
+            })
+            .catch((err) => {
+                console.log(err);
+            })
 
             pokemon_img.addEventListener('mouseover', () => {
                 pokemon_img.src = urlById(i, true);
@@ -144,6 +128,26 @@ function generatePokemon(min, max) {
     }
 }
 
+function generateIndividualPokemon(index) {
+    const main = document.querySelector('body');
+    const container = document.createElement('div');
+    const row = document.createElement('div');
+    const img = document.createElement('img');
+    const pokemon_name = document.createElement('h2');
+
+    img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/${index}.gif`
+    pokemon_name.textContent = 
+
+    container.classList.add('container');
+    row.classList.add('row', 'desc');
+    img.classList.add('col-6');
+    pokemon_name.classList.add('col-6');
+
+    main.appendChild(container);
+    container.appendChild(row);
+    row.appendChild(img);
+}
+
 function urlById(id, isShiny = false) {
     if (isShiny) {
         return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${id}.png`
@@ -160,6 +164,19 @@ window.addEventListener('load', () => {
     const title = document.querySelector('h1');
 
     generatePokemon(1, 151);
+    const pokemon_img = document.querySelectorAll('img');
+
+    if (pokemon_img) {
+        for (const img of pokemon_img) {
+            img.addEventListener('click', () => {
+                console.log(img)
+                let link = img.src;
+                let index = link.match(/(\d+)/);
+                clearAll();
+                generateIndividualPokemon(parseInt(index[0]));
+            })
+        }
+    }
 
     if (inputBox) {
         inputBox.addEventListener('input', () => {
