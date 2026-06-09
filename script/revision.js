8f83ce2edd6fcaaa4f9738ad7c1d57ea38330104
Kévin Tourret <kevin@drosalys.fr>
2026-06-09 12:02:38 +0200

    add correction exo revision

--- script/revision.js
+++ script/revision.js
@@ -8,27 +8,34 @@ window.addEventListener('load', () => {
 
 function loadDataTable(tables) {
     for (const table of tables) {
+
+        const tableLines = table.querySelectorAll('tbody > tr');
+
         // Création du container de l'input avec l'input, pour chaque table HTML récupérée
         const div = createElementByTagName('div', 'd-flex');
         const input = createElementByTagName('input', 'form-control', 'w-25', 'ms-auto');
         input.type = 'search';
+        const p = createElementByTagName('p');
+        p.textContent = tableLines.length +  " élément(s) affiché(s)";
 
+        div.appendChild(p);
         div.appendChild(input);
         table.before(div);
-
-        const tableLines = table.querySelectorAll('tbody > tr');
 
         // Configuration de l'input
         input.addEventListener('input', () => {
             const value = input.value;
+            let cpt = tableLines.length;
             for (const tr of tableLines) {
                 tr.classList.remove('d-none');
                 if (value.length >= 2) {
                     if (!tr.innerHTML.toLowerCase().includes(value.toLowerCase())) {
                         tr.classList.add('d-none');
+                        cpt--;
                     }
                 }
             }
+            p.textContent = cpt + " élément(s) affiché(s)";
         });
     }
 }
